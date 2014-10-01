3b7b61b6b97185131483908661380f3579bd64a8
unknown <kevin.roberts@KROBERTS-LAPTOP.office.selerix.com>
2014-10-01 15:34:16 -0500

    Added Stats to wrestler object

--- wrestest.py
+++ wrestest.py
@@ -2,6 +2,7 @@ import webapp2
 import randomName
 import randomCityAndState
 import randomHeightAndWeight
+import wrestlerStats
 
 class MainPage(webapp2.RequestHandler):
 
@@ -26,11 +27,31 @@ class Wrestler(Worker):
     def __init__(self):
         Worker.__init__(self)
         self.type = "Wrestler"
-
+        self.style = wrestlerStats.assignStyle(self.weight)
+        self.strength, self.aerial, self.grappling, self.striking, self.movement \
+                       = wrestlerStats.assignPhysicalStats(self.style)
+        self.microphone, self.look, self.charisma, self.selling, self.psychology \
+                       = wrestlerStats.assignEntertainmentStats()
+        self.overall = wrestlerStats.assignOverall(self.style, self.strength, self.aerial, self.grappling,
+                                                   self.striking, self.movement, self.microphone, self.look,
+                                                   self.charisma, self.selling, self.psychology)
+    
     def __str__(self):
-        return Worker.__str__(Worker()) + \
-               str(self.type)
-        
+        return Worker.__str__(self) + \
+               "Type:       " + str(self.type) + '\n' + \
+               "Style:      " + str(self.style) + '\n' + \
+               "Overall:    " + str(self.overall) + '\n' + \
+               "Strength:   " + str(self.strength) + '\n' + \
+               "Aerial:     " + str(self.aerial) + '\n' + \
+               "Grappling:  " + str(self.grappling) + '\n' + \
+               "Striking:   " + str(self.striking) + '\n' + \
+               "Movement:   " + str(self.movement) + '\n' + \
+               "Microphone: " + str(self.microphone) + '\n' + \
+               "Look:       " + str(self.look) + '\n' + \
+               "Charisma:   " + str(self.charisma) + '\n' + \
+               "Selling:    " + str(self.selling) + '\n' + \
+               "Psychology: " + str(self.psychology)
+    
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
